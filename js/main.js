6400d5285f18153ddeea3505b063f759c55d482f
Igor Levin <levin.igor90@bk.ru>
2020-06-03 22:40:35 +0300

    Migrate to Mac

--- js/main.js
+++ js/main.js
@@ -1,23 +1,21 @@
 'use strict';
 
-function getRandomInt(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min)) + min; //  Максимум не включается, минимум включается
-}
-
 var flatType = ['palace', 'flat', 'house', 'bungalo'];
 var checkinTime = ['12:00', '13:00', '14:00'];
 var checkoutTime = ['12:00', '13:00', '14:00'];
 var featuresType = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
-var photos = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
+var photos = [
+  'http://o0.github.io/assets/images/tokyo/hotel1.jpg',
+  'http://o0.github.io/assets/images/tokyo/hotel2.jpg',
+  'http://o0.github.io/assets/images/tokyo/hotel3.jpg'
+];
 
-for (var i = 1; i <= 8; i++) {
+for (var i = 0; i < 8; i++) {
   var arr = featuresType.slice(getRandomInt(0, 6), 6);
   var arr2 = photos.slice(getRandomInt(0, 3), 3);
   var newAdvertisement = {
     'author': {
-      'avatar': 'img/avatars/user' + i + '.png',
+      'avatar': 'img/avatars/user0' + i + '.png',
     },
     'offer': {
       'title': 'Предложение ' + i,
@@ -43,3 +41,8 @@ for (var i = 1; i <= 8; i++) {
 var mapActive = document.querySelector('.map');
 mapActive.classList.remove('map--faded');
 
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min; //  Максимум не включается, минимум включается
+}
